a2bcfff924bee16f306e688e268af94c4fb8d7ac
vigneshwaran26 <vicky.ravi26@gmail.com>
2022-07-03 14:52:33 +0530

    fastify middleware added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
-import { ValidationPipe } from '@nestjs/common'
+import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
+  const app = await NestFactory.create<NestFastifyApplication>(MessagesModule,new FastifyAdapter());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
