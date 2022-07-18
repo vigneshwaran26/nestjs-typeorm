d2cc88cbbe39839d9d3a96b276987e7ca04a8402
plantd-develop <plantd.dev12@tringapps.com>
2022-07-18 16:22:13 +0530

    controllers for messages added

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
 import { MessagesRepository } from './messages.repository';
 import { MessagesService } from './messages.services';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
+  imports:[
+    // TypeOrmModule.forRoot({
+    // type:'postgres',
+    // database:'TringConnect',
+    // entities:[],
+    // migrations:[],
+    // synchronize: process.env.DB_SYNC === 'false',
+  // })
+],
   controllers: [MessagesController],
   providers:[MessagesService,MessagesRepository]
 })
